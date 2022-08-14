134a6a698a717b3b9799d39633d0f8159a551722
abo3adel <ahmed.adel@212solutionsllc.com>
2022-08-14 17:14:02 +0100

    get favoured attribute per item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,6 +8,8 @@ use App\Models\Category;
 use App\Models\DoughType;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class Item extends Model
 {
@@ -16,7 +18,7 @@ class Item extends Model
     protected $fillable = ['name_ar', 'name_en', 'price', 'calories', 'category_id', 'description_ar', 'description_en', 'image'];
 
     protected $hidden = ["branches"];
-    protected $appends = ['is_hidden', 'dough_type'];
+    protected $appends = ['is_hidden', 'dough_type', 'favoured'];
 
     public function category()
     {
@@ -76,6 +78,15 @@ class Item extends Model
         $dough_type = DoughType::where('dough_type_id', $dough_type_id)->select('name_ar', 'name_en')->get();
 
         return $dough_type;
+    }
+
+    public function getFavouredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+
+        return DB::table('favourite_item')->where('user_id', Auth::id())->where('item_id', $this->id)->exists();
     }
 
     public function isVisibleForAuthUser()
