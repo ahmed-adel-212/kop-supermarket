958196694179f632256d3cc60b0eaa6f02525bc0
abo3adel <ahmed.adel@212solutionsllc.com>
2022-08-17 11:35:36 +0100

    allow admin to toggle offers on homepage

--- app/Http/Controllers/Admin/OfferController.php
+++ app/Http/Controllers/Admin/OfferController.php
@@ -395,6 +395,16 @@ class OfferController extends Controller
 
     public function setAsMain(Request $request, Offer $offer)
     {
+        // check if main offers more than two
+        $count = Offer::where('main', true)->count();
+
+        if ($count >= 2) {
+            return  redirect()->route('admin.offer.index')->with([
+                'type' => 'error',
+                'message' => 'you can not add more than 2 offers at homepage'
+            ]);
+        }
+
         $offer->main = true;
         $offer->save();
 
--- resources/views/admin/offer/index.blade.php
+++ resources/views/admin/offer/index.blade.php
@@ -1,91 +1,111 @@
 @extends('layouts.admin.app')
 @section('content')
-<div class="content-wrapper">
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Offers</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{route('admin.offer.create')}}">Add New Offer</a></li>
-          </ol>
-        </div>
-      </div>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Offers</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.offer.create') }}">Add New Offer</a></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="card-body">
+                    <table class="table table-bordered table-striped dataTable">
+                        <thead>
+                            <tr>
+                                <th>#ID</th>
+                                <th>Name</th>
+                                <th>Date From</th>
+                                <th>Date To</th>
+                                <th>Service</th>
+                                <th>Image</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($offers as $offer)
+                                <tr>
+                                    <td><a href="{{ route('admin.offer.show', $offer->id) }}">{{ $offer->id }}</a></td>
+                                    <td>{{ $offer->title }}</td>
+                                    <td>{{ $offer->date_from }}</td>
+                                    <td>{{ $offer->date_to }}</td>
+                                    <td>{{ $offer->service_type }}</td>
+                                    <td style="max-width:50px" class="text-center"><img class="img-fluid"
+                                            src="{{ $offer->image }}" /></td>
+                                    <td>
+                                        @if ($offer->main)
+                                            <form action="{{ route('admin.offer.unmain', $offer->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm"
+                                                    title="show on home screen">
+                                                    <i class="fas fa-home"></i>
+                                                </button>
+                                            </form>
+                                        @else
+                                            <form action="{{ route('admin.offer.main', $offer->id) }}" method="POST">
+                                                @csrf
+                                                @method('PUT')
+                                                <button type="submit" class="btn btn-success btn-sm"
+                                                    title="show on home screen">
+                                                    <i class="fas fa-home"></i>
+                                                </button>
+                                            </form>
+                                        @endif
+
+                                        <a href="{{ route('admin.offer.edit', $offer->id) }}"
+                                            class="btn btn-primary btn-circle btn-sm" title="edit"><i
+                                                class="fa fa-edit"></i></a>
+                                        <a onclick="deleteOffer('{{ 'delete-offer-' . $offer->id }}')" href="#"
+                                            class="btn btn-danger btn-circle btn-sm" title="delete">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                        <!-- Form Delete offer -->
+                                        <form action="{{ route('admin.offer.destroy', $offer->id) }}" method="POST"
+                                            id="{{ 'delete-offer-' . $offer->id }}">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                        <!-- End Delete offer -->
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     </div>
-  </section>
-  <section class="content">
-    <div class="container-fluid">
-      <div class="card-body">
-        <table class="table table-bordered table-striped dataTable">
-          <thead>
-            <tr>
-              <th>#ID</th>
-              <th>Name</th>
-              <th>Date From</th>
-              <th>Date To</th>
-              <th>Service</th>
-              <th>Image</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($offers as $offer)
-            <tr>
-              <td><a href="{{ route('admin.offer.show', $offer->id) }}">{{ $offer->id }}</a></td>
-              <td>{{$offer->title}}</td>
-              <td>{{$offer->date_from}}</td>
-              <td>{{$offer->date_to}}</td>
-              <td>{{$offer->service_type}}</td>
-              <td style="max-width:50px" class="text-center"><img class="img-fluid" src="{{$offer->image}}"/></td>
-              <td>
-                  <a href="{{ route('admin.offer.edit', $offer->id) }}" class="btn btn-primary btn-circle btn-sm" title="edit"><i class="fa fa-edit"></i></a>
-                  <a onclick="deleteOffer('{{ 'delete-offer-' . $offer->id }}')"
-                     href="#" class="btn btn-danger btn-circle btn-sm"
-                     title="delete">
-                      <i class="fas fa-trash"></i>
-                  </a>
-                  <!-- Form Delete offer -->
-                  <form
-                      action="{{ route('admin.offer.destroy', $offer->id) }}"
-                      method="POST"
-                      id="{{ 'delete-offer-' . $offer->id }}">
-                      @csrf
-                      @method('DELETE')
-                  </form>
-                  <!-- End Delete offer -->
-                </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </section>
-</div>
 @endsection
 @push('js')
-<script type="text/javascript">
-  function deleteOffer(id) {
-    event.preventDefault();
-    swal({
-        title: 'Are you sure to delete this offer ?',
-        text: 'Once the offer has been deleted you cannot retrieve its data',
-        icon: 'warning',
-        buttons: true,
-        dangerMode: true,
-    })
-    .then((willDelete) => {
-        if (willDelete) {
-            $('#' + id).submit();
-            swal('Offer successfully deleted', {
-                icon: 'success',
-            });
-        } else {
-            swal('Offer undeleted');
+    <script type="text/javascript">
+        function deleteOffer(id) {
+            event.preventDefault();
+            swal({
+                    title: 'Are you sure to delete this offer ?',
+                    text: 'Once the offer has been deleted you cannot retrieve its data',
+                    icon: 'warning',
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        $('#' + id).submit();
+                        swal('Offer successfully deleted', {
+                            icon: 'success',
+                        });
+                    } else {
+                        swal('Offer undeleted');
+                    }
+                });
         }
-    });
-  }
-</script>
+    </script>
 @endpush
