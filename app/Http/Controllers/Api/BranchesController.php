7c0ec3bfa20620341a69db4c451070fe1a522e0c
noura-samy <noura.samy@212solutionsllc.com>
2022-09-05 17:11:01 +0200

    date branch

--- app/Http/Controllers/Api/BranchesController.php
+++ app/Http/Controllers/Api/BranchesController.php
@@ -65,7 +65,15 @@ class BranchesController extends BaseController
         ];
         
         for($i=0;$i<count($open);$i++)
-        {    if((strtotime($open[$i]) < time()) and (strtotime($close[$i]) >  time()))
+        {   
+            $date=date("Y-m-d");
+            $o= date('Y-m-d H:i:s', strtotime("$date $open[$i]"));
+            if(str_contains($close[$i],"AM"))
+            {
+                 $date = date("Y-m-d", strtotime("+1 day"));
+            }
+            $c= date('Y-m-d H:i:s', strtotime("$date $close[$i]"));
+            if((strtotime(date("Y-m-d H:i",strtotime($o))) < time()) and (strtotime(date("Y-m-d H:i",strtotime($c))) >  time()))
             {
                 $data['available']=true;
                 return $this->sendResponse($data,__('general.branch_ret'));
