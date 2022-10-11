fa7f72c78ccd8f5f3f9b7f17a619a716cb4f33c7
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-11 16:44:16 +0100

    fix brand image has no base url

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -15,4 +15,13 @@ class Brand extends Model
     {
         return $this->hasMany(Item::class);
     }
+
+    public function getImageAttribute($value)
+    {
+        if (!empty($value) && file_exists(public_path($value))) {
+            return url($value);
+        } else {
+            return 'http://via.placeholder.com/200x200?text=No+Image';
+        }
+    }
 }
